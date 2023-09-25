ebacfb7b6c8347161ccad2abd17ab4b42961ab44
Patton Tang <pattontanges@gmail.com>
2023-09-25 16:40:16 -0700

    Main.py file with initial intended structure, IO problem statement, and preliminary test print

---
+++ main.py
@@ -0,0 +1,20 @@
+#from FILENAME import CLASSNAME
+#import FILENAME1
+#import FILENAME2
+
+'''
+>> Algorithm Structure for MeetingTime problem instance:
+Find / Read Filepath
+Create Data Structures
+Read & Parse File(Data Structures)
+Identify & Output(Data Structures) # can be split in two if output data is saved
+
+I/O:
+I - A specifically formatted txt file with two person's schedules written in military time without conflicts and a specified minimum meeting time
+O - A txt file listing the available times between both persons with a [[XX:XX], [YY:YY]] format
+
+The main file above should be written with the functions written in a different file for readability.
+'''
+
+if __name__ == "__main__":
+    print("Python branch works!")
